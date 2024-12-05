8a7a7f626f5f083d75f8820559e7791c7de6138e
admin deepfake <hm@bigscale.fr>
2024-12-05 22:36:14 +0100

    first commit

---
+++ src/main/java/com/example/jenkins_helloworld/JenkinsHelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkins_helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHelloworldApplication.class, args);
+		System.out.println("Hello wolrd");
+	}
+
+}
---
+++ src/test/java/com/example/jenkins_helloworld/JenkinsHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
