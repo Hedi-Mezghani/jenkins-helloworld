47a5b365efbe269e6fe3e382d528a4928c7758ab
Hedi-Mezghani <94811429+Hedi-Mezghani@users.noreply.github.com>
2024-12-06 16:39:40 +0100

    Update JenkinsHelloworldApplication.java

--- src/main/java/com/example/jenkins_helloworld/JenkinsHelloworldApplication.java
+++ src/main/java/com/example/jenkins_helloworld/JenkinsHelloworldApplication.java
@@ -8,7 +8,7 @@ public class JenkinsHelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsHelloworldApplication.class, args);
-		System.out.println("Hello wolrd");
+		System.out.println("Hello rabi3 bou den");
 	}
 
 }
